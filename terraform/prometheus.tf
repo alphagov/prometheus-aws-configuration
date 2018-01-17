74ff3316569060e39930d9f5a630dd0f89071356
Chris Patuzzo <chris@patuzzo.co.uk>
2018-01-17 15:37:23 +0000

    Create an EC2 instance running Ubuntu within our subnet

---
+++ terraform/prometheus.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "prometheus" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.main.id}"
+
+  tags {
+    Name = "Prometheus"
+  }
+}
