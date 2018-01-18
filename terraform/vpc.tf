4f4a34b1e9ec9fed17c443d5f94760338c0df72e
Chris Patuzzo <chris@patuzzo.co.uk>
2018-01-18 11:17:57 +0000

    Assign ip addresses and domain names to instances in the VPC

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/24"
+  enable_dns_hostnames = true
 
   tags {
     Name = "Reliability Engineering - Prometheus VPC"
@@ -9,6 +10,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id     = "${aws_vpc.main.id}"
   cidr_block = "${aws_vpc.main.cidr_block}"
+  map_public_ip_on_launch = true
 
   tags {
     Name = "Main"
