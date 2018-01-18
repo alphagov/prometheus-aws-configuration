45edd2ed4f9fccb685f8b5d9de9fe8c0120737dd
Chris Patuzzo <chris@patuzzo.co.uk>
2018-01-18 11:20:10 +0000

    Connect the VPC to the Internet via a gateway

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,6 +7,14 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    Name = "main"
+  }
+}
+
 resource "aws_subnet" "main" {
   vpc_id     = "${aws_vpc.main.id}"
   cidr_block = "${aws_vpc.main.cidr_block}"
@@ -32,4 +40,18 @@ resource "aws_security_group" "ssh_from_internet" {
   tags {
     Name = "SSH from internet"
   }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = "${aws_subnet.main.id}"
+  route_table_id = "${aws_route_table.public.id}"
 }
