48d989c975f02ff282c7b83b25530913111f5f26
Chris Patuzzo <chris@patuzzo.co.uk>
2018-01-18 11:18:48 +0000

    Add the ‘ssh_from_internet’ rule with GDS whitelisted ips

--- terraform/prometheus.tf
+++ terraform/prometheus.tf
@@ -15,9 +15,10 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "prometheus" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-  subnet_id     = "${aws_subnet.main.id}"
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  instance_type          = "t2.micro"
+  subnet_id              = "${aws_subnet.main.id}"
+  vpc_security_group_ids = ["${aws_security_group.ssh_from_internet.id}"]
 
   tags {
     Name = "Prometheus"
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -16,3 +16,20 @@ resource "aws_subnet" "main" {
     Name = "Main"
   }
 }
+
+resource "aws_security_group" "ssh_from_internet" {
+  vpc_id      = "${aws_vpc.main.id}"
+  name        = "SSH from internet"
+  description = "Bastion hosts in the DMZ that can be connected to via SSH from whitelisted locations"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["${var.cidr_admin_whitelist}"]
+  }
+
+  tags {
+    Name = "SSH from internet"
+  }
+}
