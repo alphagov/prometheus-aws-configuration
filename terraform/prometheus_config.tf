9680173e7af3e77cf8122c4655dae964aa10a515
Chris Patuzzo <chris@patuzzo.co.uk>
2018-01-19 15:23:49 +0000

    Configure Prometheus to pull metrics from PaaS apps

--- terraform/prometheus_config.tf
+++ terraform/prometheus_config.tf
@@ -13,3 +13,10 @@ resource "aws_s3_bucket_object" "prometheus_config" {
   source = "prometheus.yml"
   etag   = "${md5(file("prometheus.yml"))}"
 }
+
+resource "aws_s3_bucket_object" "prometheus_config_targets" {
+  bucket = "${aws_s3_bucket.prometheus_config_bucket.id}"
+  key    = "targets/paas_sample_apps.json"
+  source = "paas_sample_apps.json"
+  etag   = "${md5(file("paas_sample_apps.json"))}"
+}
