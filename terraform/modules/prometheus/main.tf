3446b5acb626246fb19704c84d20f296780bdf1c
David King <david.king@digital.cabinet-office.gov.uk>
2018-01-26 15:02:02 +0000

    Add HTTP on port 80 back in
    nginx has been configured to redirect to HTTPS and port 80 is being used to
    initially get the certificate from Let's Encrypt.

--- terraform/modules/prometheus/main.tf
+++ terraform/modules/prometheus/main.tf
@@ -191,6 +191,13 @@ resource "aws_security_group" "external_http_traffic" {
 
   ingress {
     protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
     from_port   = 443
     to_port     = 443
     cidr_blocks = ["0.0.0.0/0"]
