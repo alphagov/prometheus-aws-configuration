a7fa99bb04896e899d0be522c328ee0781a34f20
David Jeche <davidjeche@gds8091.local>
2018-03-12 17:53:03 +0000

    removed ssh cidr block in alertmanager

--- terraform/modules/alertmanager/variables.tf
+++ terraform/modules/alertmanager/variables.tf
@@ -1,19 +1,3 @@
-# See https://sites.google.com/a/digital.cabinet-office.gov.uk/gds-internal-it/news/aviationhouse-sourceipaddresses for details.
-variable "cidr_admin_whitelist" {
-  description = "CIDR ranges permitted to communicate with administrative endpoints"
-  type        = "list"
-
-  default = [
-    "213.86.153.212/32",
-    "213.86.153.213/32",
-    "213.86.153.214/32",
-    "213.86.153.235/32",
-    "213.86.153.236/32",
-    "213.86.153.237/32",
-    "85.133.67.244/32",
-  ]
-}
-
 variable "ami_id" {}
 
 variable "alertmanager_version" {
